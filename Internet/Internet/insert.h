c56985b4459bbbf16fbcf1229d30750adc83d7f3
guojinyu12 <1765221348@qq.com>
2019-12-24 10:03:39 +0800

    ?

--- Internet/Internet/insert.h
+++ Internet/Internet/insert.h
@@ -1,7 +1,9 @@
 #include"Graphlink.h"
 template<typename T>
-inline bool Graphlink<T>::insertEdge(int v1, int v2,int cost)
+inline bool Graphlink<T>::insertEdge(int vertex1, int vertex2,int cost)
 {
+	int v1 = getVertexPos(vertex1);
+	int v2 = getVertexPos(vertex2);
 	Table[v1].adj = new Edge(v2, cost, Table[v1].adj);
 	if(Table[v1].adj==nullptr)
 		return false;
